c598caeafd330582bf33760ebf97f4d8c03c4231
shruti garg <hellocoders8213@gmail.com>
2024-01-27 18:04:39 +0530

    build navbar

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,25 @@
+const Hero = () => {
+	return (
+		<div className="hero">
+			<div className="content">
+				<h1>YOUR FEET DESERVE THE BEST</h1>
+				<p>
+					YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+					SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+					SHOES.
+				</p>
+				<div className="btn">
+					<button>Shop Now</button>
+					<button>Category</button>
+				</div>
+				<div className="shopping">Also Available On</div>
+			</div>
+			<div className="img">
+				<img src="images/flipkart.png"></img>
+				<img src="images/amazon.png"></img>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
