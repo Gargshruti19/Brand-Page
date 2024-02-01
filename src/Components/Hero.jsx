80f5f14dd873862075734e039a789e9546a2dad3
shruti garg <hellocoders8213@gmail.com>
2024-02-01 13:30:40 +0530

    done half responsiveness of hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,10 @@
 const HeroSection = () => {
 	return (
-		<div className="hero container">
+		<div className="hero">
 			<div className="hero-content">
-				<h1>YOUR FEET DESERVE THE BEST</h1>
+				<h1>
+					YOUR FEET DESERVE <br /> THE BEST
+				</h1>
 				<p>
 					YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
 					SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
@@ -16,7 +18,7 @@ const HeroSection = () => {
 					<p>Also Available On</p>
 					<div className="brand-icons">
 						<img src="images/flipkart.png" alt="amazon-logo"></img>
-						<img src="images/amazon.png" alt="flipcart-logo"></img>
+						<img src="images/amazon.png" alt="flipkart-logo"></img>
 					</div>
 				</div>
 			</div>
